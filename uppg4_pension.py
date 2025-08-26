cd0f4746632f2f52f1501f16bbf9c641e162eec7
Feisel1 <feimah081007@student.jenseneducation.se>
2025-08-26 11:33:52 +0200

    klar

--- uppg4_pension.py
+++ uppg4_pension.py
@@ -6,3 +6,10 @@ Räkna med att man går i pension vid 67 års ålder.
 Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Hej och välkommen till mitt program (Hampus). Du har (39) år kvar till pension.
 """
+name =input("what is yoour name?:")
+age =input("how old are you?:")
+
+retirement_age = 67
+
+age_left = retirement_age - int(age)
+print(f"hi {name} you have {age_left} years left to retirement")
