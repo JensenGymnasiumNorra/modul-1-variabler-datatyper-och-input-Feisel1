b6cf8eb08f19aebd10823b2f45f74e20ba41666c
Feisel1 <feimah081007@student.jenseneducation.se>
2025-08-28 00:05:25 +0200

    klar

--- uppg6_adder.py
+++ uppg6_adder.py
@@ -6,3 +6,8 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 (5) + (3) = (8)
 
 """
+import random
+a = int(input("choose a number: "))
+b = int(input("choose another number: "))
+c = a + b
+print(f"{a} + {b} = {c}")
