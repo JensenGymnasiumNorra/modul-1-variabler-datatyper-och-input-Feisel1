60048a9f8d0f7300f09d0a6a237a14c4c9f9fb5a
Feisel1 <feimah081007@student.jenseneducation.se>
2025-08-27 20:37:49 +0200

    done

--- uppg5_more_variables.py
+++ uppg5_more_variables.py
@@ -12,3 +12,10 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Välkommen till mitt program (Oskar). Du och (Hampus) är (53) år tillsammans.
 
 """
+
+his_name =input("What is his name?: ")
+his_age =input("How old is he?: ")
+your_name = input("What is your name?: ")
+your_age = input("How old are you?: ")
+total_age = int(his_age) + int(your_age)
+print(f"Welcome to my program {your_name} and {his_name}. You guys is {total_age} years old together.")
