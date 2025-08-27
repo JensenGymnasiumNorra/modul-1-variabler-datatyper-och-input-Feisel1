72b00faa9a2da9d54005cd7d380525c05cf94667
Feisel1 <feimah081007@student.jenseneducation.se>
2025-08-28 00:12:38 +0200

    klar (råkade blanda uppgift 6 och 7)

--- uppg7_square.py
+++ uppg7_square.py
@@ -5,3 +5,9 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 (3) i kvadrat är (9). 
 
 """
+import random
+
+a= random.randint(1,10)
+b= random.randint(1,10)
+c= a * b
+print(f"{a} * {b} är {c}")
